8d45321fc11ca3de72f115e1c7ff988f8626dc91
Gadivalr <levi.sojos7@gmail.com>
2023-07-07 16:25:24 -0400

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,23 @@
-
 (() => {
+  // Theme switch
   const body = document.body;
   const lamp = document.getElementById("mode");
 
-  const toggleTheme = () => {
-    localStorage.setItem("theme", "dark");
-    body.setAttribute("data-theme", "dark");
-    
-    // Actualizar Disqus después de cambiar el tema
+  const toggleTheme = (state) => {
+    if (state === "dark") {
+      localStorage.setItem("theme", "light");
+      body.removeAttribute("data-theme");
+    } else if (state === "light") {
+      localStorage.setItem("theme", "dark");
+      body.setAttribute("data-theme", "dark");
+    } else {
+      initTheme(state);
+    }
   };
 
-  toggleTheme(); // Establecer el tema oscuro de forma predeterminada al cargar la página
-
-  lamp.addEventListener("click", () => toggleTheme( 
-    window.location.href = "/" // Actualizar la página después de cambiar el tema
-  )
-      
+  lamp.addEventListener("click", () =>
+    toggleTheme(localStorage.getItem("theme"))
   );
-  
 
   // Blur the content when the menu is open
   const cbox = document.getElementById("menu-trigger");
@@ -31,16 +31,32 @@
 })();
 
 // Obtén la casilla de verificación del menú
-var menuTrigger = document.getElementById('menu-trigger');
-
-// Obtén todos los enlaces del menú
-var menuLinks = document.querySelectorAll('.menu-link');
 
 // Agrega un controlador de eventos a cada enlace del menú
-menuLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
+//menuLinks.forEach(function(link) {
+  //link.addEventListener('click', function() {
     // Desmarca la casilla de verificación del menú
+    //menuTrigger.removeAttribute('checked');
+    //menuTrigger.checked = false;
+    // menuTrigger.click();
+  //});
+//});
+// menú desactivado cuando se navega a otra página
+// window.addEventListener('beforeunload', function() {
+//    // Desmarca la casilla de verificación del menú
+//    var menuTrigger = document.getElementById('menu-trigger');
+//    menuTrigger.checked = false;
+//  });
+document.addEventListener('visibilitychange', function() {
+  var menuTrigger = document.getElementById('menu-trigger');
+  
+  if (document.visibilityState === 'hidden') {
+    // Cambiar a otra pestaña
     menuTrigger.checked = false;
-  });
+  } else {
+    // Regresar a la pestaña actual
+    menuTrigger.disabled = false;
+  }
 });
+
 
