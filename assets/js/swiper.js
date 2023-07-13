0f61117ac7ea740118261256dcdd19208a8d8100
Gadivalr <levi.sojos7@gmail.com>
2023-07-12 22:47:55 -0400

    des

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,5 +1,6 @@
 const slides = document.querySelectorAll("[data-slide]");
 const buttons = document.querySelectorAll("[data-button]");
+const descriptionElements = document.querySelectorAll(".description");
 
 let currSlide = 0;
 let maxSlide = slides.length - 1;
@@ -7,6 +8,14 @@ let maxSlide = slides.length - 1;
 const updateCarousel = (number = 0) => {
   slides.forEach((slide, index) => {
     slide.style.transform = `translateX(${(index - number) * 100}%)`;
+  });
+
+  descriptionElements.forEach((description, index) => {
+    if (index === number) {
+      description.style.opacity = 1;
+    } else {
+      description.style.opacity = 0;
+    }
   });
 };
 
